a97685662caca576e31bf48928fcdf0868cfff23
Joe S <schnetzlerjoe@gmail.com>
2021-07-21 01:28:48 -0500

    Initial commit

---
+++ src/lib/Action.svelte
@@ -0,0 +1,45 @@
+<script>
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
+  />
+  <script src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</svelte:head>
+
+<div class="action">
+    <div class="box mainBox">
+        <div class="columns">
+            <div class="column is-12">
+                <h2 class="content functionHeader">
+                    Function
+                </h2>
+                <p class="content functionExplainer">
+                    Scrape url specified and then find element by id.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+.action {
+    width: 100%;
+}
+.mainBox {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+}
+.functionHeader {
+    font-size: 20px;
+    text-align: center;
+}
+.functionExplainer {
+    font-size: 16px;
+    text-align: center;
+}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Action from '../lib/Action.svelte'
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"
+  />
+  <script src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+</svelte:head>
+
+<div class="columns">
+	<div class="column is-2">
+		<Action></Action>
+	</div>
+	<div class="column is-2">
+		<Action></Action>
+	</div>
+	<div class="column is-2">
+		<Action></Action>
+	</div>
+	<div class="column is-2">
+		<Action></Action>
+	</div>
+	<div class="column is-2">
+		<Action></Action>
+	</div>
+	<div class="column is-2">
+		<Action></Action>
+	</div>
+</div>
+
+<style>
+</style>
